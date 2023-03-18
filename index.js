da38d605325ad5605ac9c04263a706440f17a057
Rostyslav Vendysh <rosvenvas@gmail.com>
2023-03-18 12:45:50 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const array = [1, 2, 3, 4, 5]
+const array2 = [2, 4]
 
 // Checks whether an element is even
 const even = (element) => element % 2 === 0
@@ -6,4 +7,5 @@ const even = (element) => element % 2 === 0
 const odd = (element) => element % 2 !== 0
 
 console.log(array.some(even))
-console.log(array.some(odd))
+
+console.log(array2.some(odd))
